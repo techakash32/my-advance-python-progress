1cf0d51c6ed0c0873e3eac6d4161caf464d34faf
Akash Nagar <148152078+techakash32@users.noreply.github.com>
2025-12-19 10:58:29 +0530

    Add files via upload

---
+++ QUESTIONS/8TH.py
@@ -0,0 +1,7 @@
+#Flatten a 2D array into 1D using both flatten() and ravel(). Observe the
+#difference.
+import numpy as np
+a=[[1,2,3],[4,5,6],[7,8,9]]
+x=np.array(a)
+array15=x.flatten()
+print(array15)
